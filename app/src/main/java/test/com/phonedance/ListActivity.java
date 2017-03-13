779939b36feacdcae20091e1a46a42e4045de4cd
Nirmal <ksnirm@gmail.com>
2017-03-13 11:46:29 +0530

    list screen

--- app/src/main/java/test/com/phonedance/ListActivity.java
+++ app/src/main/java/test/com/phonedance/ListActivity.java
@@ -2,10 +2,15 @@ package test.com.phonedance;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
 
+    ArrayList<String> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,10 +18,20 @@ public class ListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        setList();
+        if(getList()){
+            setList();
+        }
+    }
+
+    private boolean getList() {
+
+
+        return false;
     }
 
     private void setList() {
+        RecyclerView list = (RecyclerView) findViewById(R.id.gesture_list);
+        list.setVisibility(View.VISIBLE);
 
     }
 
