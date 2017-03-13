cb4a4ed10e14987f1774907b2fc01f8dd46513bd
Nirmal <ksnirm@gmail.com>
2017-03-13 11:05:24 +0530

    after build change

--- app/src/main/java/test/com/phonedance/MainActivity.java
+++ app/src/main/java/test/com/phonedance/MainActivity.java
@@ -1,5 +1,6 @@
 package test.com.phonedance;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -27,9 +28,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -44,7 +42,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button_new:
-
+                startActivity(new Intent(this,SecondActivity.class));
                 break;
 
             case R.id.button_test:
